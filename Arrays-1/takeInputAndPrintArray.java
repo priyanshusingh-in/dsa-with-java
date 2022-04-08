87f06546397fb81f78f43598e31ae863fc3963ad
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-04-08 07:44:49 +0530

    Update takeInputAndPrintArray.java
    Added method 2 which involves the use of functions

--- Arrays-1/takeInputAndPrintArray.java
+++ Arrays-1/takeInputAndPrintArray.java
@@ -23,8 +23,8 @@ printing the array
 import java.util.*;
 class takeInputAndPrintArray
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
+ public static void main (String[] args) throws java.lang.Exception
+ {
 	  Scanner sc=new Scanner(System.in);
 	  System.out.println("set size of array");
       int arrSize=sc.nextInt();
@@ -41,5 +41,44 @@ class takeInputAndPrintArray
       {   
           System.out.print(arr[i]+" | ");
       }
+ }
+}
+
+
+//METHOD 2 (USING FUNCTIONS)
+
+import java.util.*;
+class takeInputAndPrintArray
+{
+	public static int[] takeInput()
+	{
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("enter size of array");
+	    int arrSize=sc.nextInt();
+	    int[] arr=new int[arrSize];
+	    for(int i=0;i<arrSize;i++)
+	    {
+	         System.out.println("enter value for "+i+"th index");
+	         arr[i]=sc.nextInt();
+	    }
+	    return arr;
+	}
+	
+	public static void printArray(int arr[])
+	{
+	  int arrSize=arr.length;
+	  System.out.println("printing the array");
+      System.out.print("| ");
+      for(int i=0;i<arrSize;i++)
+      {   
+          System.out.print(arr[i]+" | ");
+      }
+	}
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int[] arr=takeInput();
+          printArray(arr);
+      
 	}
 }
